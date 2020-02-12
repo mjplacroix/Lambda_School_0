d9051b3096f4d57418a1cedde7a592b13c5b8d6a
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-02-12 11:38:25 -0500

    Change url

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setup(
     name="my-lambdata-1",
-    # version="1.2",
-    # author="MJ Rossetti",
-    # author_email="datacreativellc@gmail.com",
-    # description="Gameplay logic for Rock-Paper-Scissors",
-    # long_description=long_description,
-    # long_description_content_type="text/markdown", # required if using a md file for long desc
-    # license="MIT",
-    # url="https://github.com/s2t2/game-utils-py",
-    # keywords="rock paper scissors game",
-    # packages=find_packages() # ["game_utils"]
+    version="0.1",
+    author="Michael LaCroix",
+    author_email="michael.jp.lacroix@gmail.com",
+    description="Practice Package for LS course",
+    long_description=long_description,
+    long_description_content_type="text/markdown", # required if using a md file for long desc
+    license="MIT",
+    url="https://github.com/mjplacroix/Lambda_School",
+    keywords="practice project",
+    packages=find_packages() # ["my_lambdata"]
 )
